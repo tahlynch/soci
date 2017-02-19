6a606419563dda50c56a27ec9627617d50a9b078
RoughChopTom <tahlynch@gmail.com>
2017-02-19 11:35:51 -0500

    added first-page component

--- component-app/src/app/app.module.ts
+++ component-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FirstPageComponent } from './first-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ component-app/src/app/first-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'first-page',
+  templateUrl: './first-page.component.html'
+})
+export class FirstPageComponent {
+    title = 'First Page';
+}
