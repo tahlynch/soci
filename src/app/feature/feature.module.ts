2e14e6ada45442891726906a656c696d2a085c7e
RoughChopTom <tahlynch@gmail.com>
2017-02-26 18:25:49 -0500

    app bar styling

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -6,10 +6,14 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AppBarComponent } from './app-bar/app-bar.component';
 import { FeatureComponent } from './feature.component';
 import { FeatureRoutingModule } from './feature-routing.module';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+
 @NgModule({
   imports: [
     CommonModule,
-    FeatureRoutingModule
+    FeatureRoutingModule,
+    MaterialModule
   ],
   declarations: [LandingPageComponent, AppBarComponent, FeatureComponent]
 })
