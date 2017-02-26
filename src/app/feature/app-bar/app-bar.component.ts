d2d6c5d63b83c8bd00df3128d3ac7320a322d837
RoughChopTom <tahlynch@gmail.com>
2017-02-26 17:09:12 -0500

    first cut of app bar

--- src/app/feature/app-bar/app-bar.component.ts
+++ src/app/feature/app-bar/app-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-app-bar',
+  selector: 'app-bar',
   templateUrl: './app-bar.component.html',
   styleUrls: ['./app-bar.component.css']
 })
--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -6,7 +6,6 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AppBarComponent } from './app-bar/app-bar.component';
 import { FeatureComponent } from './feature.component';
 import { FeatureRoutingModule } from './feature-routing.module';
-
 @NgModule({
   imports: [
     CommonModule,
