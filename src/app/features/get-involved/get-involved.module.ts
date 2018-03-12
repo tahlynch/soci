9885f55b577f4b1728f270d98863a0ef5ea4a5a8
RoughChopTom <tahlynch@gmail.com>
2018-03-12 18:15:46 -0500

    menu sticky

--- src/app/features/get-involved/get-involved.module.ts
+++ src/app/features/get-involved/get-involved.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatMenuModule, MatIconModule } from "@angular/material";
 
 import { GetInvolvedRoutingModule } from './get-involved-routing.module';
 import { GetInvolvedComponent } from './get-involved.component';
+import { MenuStickyDirective } from "./menu-sticky.directive";
 
 @NgModule({
   imports: [
     CommonModule,
+    MatMenuModule,
+    MatIconModule,
     GetInvolvedRoutingModule
   ],
-  declarations: [GetInvolvedComponent]
+  declarations: [GetInvolvedComponent, MenuStickyDirective],
+  exports:[MenuStickyDirective]
 })
 export class GetInvolvedModule { }
---
+++ src/app/features/get-involved/menu-sticky.directive.ts
@@ -0,0 +1,32 @@
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[sociMenuSticky]'
+})
+
+export class MenuStickyDirective {
+  private headerHeight = 0;
+  private headerElement: HTMLElement;
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    this.headerElement = document.getElementById('header');
+    this.headerHeight = this.headerElement.offsetHeight;
+  }
+
+  @HostListener('window:scroll', [])
+  private onWindowScroll() {
+    const filterWrapperElement = this.el.nativeElement.querySelector('.get-involved__menu__container');
+    const filterWrapperSpacerElement = this.el.nativeElement.querySelector('.get-involved__menu__container__spacer');
+    if (this.el.nativeElement.getBoundingClientRect().top <= this.headerHeight) {
+      this.renderer.addClass(filterWrapperElement, 'menu--stuck');
+      filterWrapperElement.style.top = this.headerHeight + 'px';
+      filterWrapperElement.style.boxShadow = '0 5px 6px -3px rgba(0,0,0,0.2)';
+      this.headerElement.style.boxShadow = 'none';
+      filterWrapperSpacerElement.style.top = this.headerHeight + 'px';
+      filterWrapperSpacerElement.style.height = filterWrapperElement.offsetHeight + 'px';
+    } else {
+      this.renderer.removeClass(filterWrapperElement, 'menu--stuck');
+      filterWrapperElement.style.boxShadow = 'none';
+      filterWrapperSpacerElement.style.height = '0';
+    }
+  }
+}
